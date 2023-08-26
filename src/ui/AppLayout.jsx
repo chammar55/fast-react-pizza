81a8ec02429d5ba8350d74325b45bd22daf659cf
chammar55 <chammar5255@gmail.com>
2023-08-26 20:32:56 +0500

    8. add loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation(); // it return a loading/idle state by using which we can place a loader to tell if data being fetched
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
       <main>
-        <Outlet /> {/* For eccessimg children routes within AppLayout */}
+        <Outlet /> {/* For accessing children routes within AppLayout */}
       </main>
       <CartOverview />
     </div>
