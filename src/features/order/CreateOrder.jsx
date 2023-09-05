52bcc7c71bfd9a0bccf9bc96940c34ad23d2487f
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-09-05 09:11:39 +0500

    11. applying Redux-Thunks

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -44,6 +45,7 @@ function CreateOrder() {
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
+  const dispatch = useDispatch();
 
   const formErrors = useActionData(); // This component (CreateOrder) is linked with Action so we can access its data here to display phone error
 
@@ -58,6 +60,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get Position</button>
 
       {/* Both way are correct , we are sending post request to create new item */}
       {/* <Form method="POST" action="/order/new"> */}
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,11 @@
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding';
 
-import { createSlice } from '@reduxjs/toolkit';
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
 
 // async function fetchAddress() {
 //   // 1) We get the user's geolocation position
@@ -22,8 +23,35 @@ import { createSlice } from '@reduxjs/toolkit';
 //   return { position, address };
 // }
 
+//👇 we are using thunks for fetching data
+// this is redux-toolkit way to use thunks
+export const fetchAddress = createAsyncThunk(
+  'user/fetchAddress', //this action type name
+  async function () {
+    // And this is the actual code that we want to execute when the upper action type dispatch
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    // 3) Then we return an object with the data that we are interested in
+    //👇 payload of the fulfilled state
+    return { position, address };
+  },
+);
+
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: '',
 };
 
 const userSlice = createSlice({
@@ -34,6 +62,21 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    //👇 These are diff reducers of thunks
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.state = 'idle';
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
